bfccfcf2ffcd04206ed4a0b68e4675ca03ecf0ab
bmfernand <bmfernand@gmail.com>
2022-12-29 14:59:11 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,34 @@
+variable "cidr" {
+  type = string
+}
+variable "public_subnet" {
+  type = string
+}
+variable "private_subnet" {
+  type = string
+}
 
+resource "aws_vpc" "main" {
+  cidr_block = var.cidr
+  tags = {
+    Name = "vpc-tf-teste"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnet
+
+  tags = {
+    Name = "Public-Subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_subnet
+
+  tags = {
+    Name = "Private-Subnet"
+  }
+}
