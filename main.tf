2d00d03e16cd9217dd2f131f04fb723adbf072b8
bmfernand <bmfernand@gmail.com>
2022-12-31 01:48:17 +0000

    merda

--- main.tf
+++ main.tf
@@ -11,16 +11,16 @@ terraform {
 
 provider "aws" {
   region = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
+//  access_key = var.access_key
+//  secret_key = var.secret_key
 }
 
 module "vpc" {
   source  = "bmfernand/vpc-test/aws"
   version = "1.0.2"
   # insert required variables here
-  cidr = var.cidr
+/*  cidr = var.cidr
   public_subnet = var.public_subnet
   private_subnet = var.private_subnet
-  vpc_name = var.vpc_name
+  vpc_name = var.vpc_name */
 }
